0b1434357d4bade8252a5532f3cf5bc14b030167
Victor Marques <vgom@cin.ufpe.br>
2016-11-20 15:47:07 -0300

    Creating method getFields

--- src/distribution/Payload.java
+++ src/distribution/Payload.java
@@ -20,6 +20,38 @@ public class Payload {
 		return content.size();
 	}
 	
+	public String toString(){
+		String r = new String();
+		
+		for(byte b:content){
+			r += (char) b;
+		}
+		
+		return r;
+	}
+	
+	public ArrayList<String> getFields(){
+		ArrayList<String> fields = new ArrayList<>();
+		String field, temp;
+		int index = 0, j, k;
+		
+		temp = this.toString();
+		
+		while(index < content.size()){
+			j = content.get(index);
+			k = content.get(index + 1);
+			index += 2;
+					
+			field = temp.substring(j+index, k+index);
+
+			fields.add(field);
+			
+			index += k;
+		}
+		
+		return fields;
+	}
+	
 	public void addField(ArrayList<Byte> field){
 		content.add((byte) 0);
 		content.add((byte) field.size());
